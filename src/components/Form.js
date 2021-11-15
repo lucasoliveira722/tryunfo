a0cd52db10bdf0c697d349bab690bf7163f0d579
Lucas Oliveira <lucasoliveira226@gmail.com>
2021-11-15 19:45:47 -0300

    Correção requisito 7

--- src/components/Form.js
+++ src/components/Form.js
@@ -144,7 +144,7 @@ class Form extends React.Component {
 
         <button
           data-testid="save-button"
-          type="submit"
+          type="button"
           disabled={ isSaveButtonDisabled }
           onClick={ onSaveButtonClick }
         >
